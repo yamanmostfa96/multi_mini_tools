26cc1f7d111728238e540a21ffcefe275b5cf786
yamanmostfa96 <yamanmostfa96@gmail.com>
2025-05-23 20:03:24 +0300

    first_commit

--- runven.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-call venv\Scripts\activate.bat
-
-REM تشغيل السكربت
-python main.py
-
-
-
-fr"C:\Users\yaman\OneDrive\3.My_Library\2.python\1.Projects\MiniTools\newenv\Scripts\activate"
--- tester.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-
-url = "https://data.humdata.org/dataset/a8b2f1dc-6428-4e86-b147-55a12d9de481/resource/80b39481-c161-4655-b2e4-677c1b259fab/download/hdx_hapi_idps_syr.csv"
-
-
-df = pd.read_csv(url)
-
-df.to_excel('results.xlsx',index=False)
